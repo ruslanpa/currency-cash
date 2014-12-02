5468523ca6713db52d14044e4e68f7274bab2315
ruslanpa <ruslan.pavlutskiy@gmail.com>
2014-12-02 12:33:31 +0200

    Extends model classes.

--- app/model.py
+++ app/model.py
@@ -2,8 +2,40 @@ __author__ = 'ruslanpa'
 
 
 class Place(object):
-    """
-    This is a feature model class
-    """
-    def __init__(self):
-        pass
+    currencies = []
+    city = 'undefined'
+
+    def __init__(self, **args):
+        for name, value in args.items():
+            if name == 'title':
+                self.title = value
+            elif name == 'address':
+                self.address = value
+            elif name == 'phone':
+                self.phone = value
+            elif name == 'cityId':
+                self.cityId = value
+            else:
+                pass
+
+
+class Currency(object):
+    def __init__(self, bid_ask=None):
+        if not bid_ask:
+            bid_ask = []
+        self.bid_ask = bid_ask
+
+    def bid(self):
+        if self.__bid_ask_exist():
+            return self.bid_ask[0]
+        else:
+            return 0
+
+    def ask(self):
+        if self.__bid_ask_exist():
+            return self.bid_ask[-1]
+        else:
+            return 0
+
+    def __bid_ask_exist(self):
+        return len(self.bid_ask) == 2
